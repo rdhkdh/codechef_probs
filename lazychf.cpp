cb94e9018f5a8a2d5bec5099620392832c2b5b41
Ridhiman Dhindsa <93422234+rdhkdh@users.noreply.github.com>
2022-04-08 10:21:20 +0530

    Add files via upload

---
+++ lazychf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while (t--)
+	{
+	    int x,m,d;
+	    cin>>x>>m>>d;
+	    if(m*x<x+d) { cout<<m*x<<endl; }
+	    else { cout<<x+d<<endl; }
+	}
+	return 0;
+}
